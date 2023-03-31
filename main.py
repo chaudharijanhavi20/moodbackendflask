0c2f5453f367b318edc3ac553ffb5692c91dede3
chaudharijanhavi20 <91479356+chaudharijanhavi20@users.noreply.github.com>
2023-03-31 06:17:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from flask import Flask, jsonify
-import os
+from deepface import DeepFace
+import cv2
+import numpy as np
+import json 
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        
+        cap = cv2.VideoCapture(0)
+        listmood=[]
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                # Draw the bounding box on the frame
+                listmood.append(frame)
+                # Display the resulting frame
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(5) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+        obj = DeepFace.analyze(img_path = listmood[-4], actions = ['gender', 'emotion'])
+        return json.dumps(obj)
+    elif request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
